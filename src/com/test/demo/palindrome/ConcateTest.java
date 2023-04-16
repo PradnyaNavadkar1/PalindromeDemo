a5dd1f92183605c67e45ffba45839f48f7bc8ad7
Pradnya Navadkar <pradnyanavadkar200@gmail.com>
2023-04-16 22:51:03 +0530

    this is java code project

---
+++ src/com/test/demo/palindrome/ConcateTest.java
@@ -0,0 +1,18 @@
+package com.test.demo.palindrome;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConcateTest {
+
+	@Test
+	void testMain() {
+		
+		PalindromeDemo demo=new PalindromeDemo();
+		System.out.println("this is palindrome string");
+		System.out.println("test is running .programme is correct");
+		
+	}
+
+}
